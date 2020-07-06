9d1bfa8a5b5190329a7d9f01d8925aa37362641b
Lucas Sousa <37560590+Lucas96Sousa@users.noreply.github.com>
2020-07-06 17:52:17 -0300

    Implementação de autenticação nas rotas

--- appbarber/src/pages/SignIn/index.tsx
+++ appbarber/src/pages/SignIn/index.tsx
@@ -7,12 +7,15 @@ import {
   Platform,
   ScrollView,
   TextInput,
+  Alert,
 } from 'react-native';
-
+import * as Yup from 'yup';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
+import getValidationErrors from '../../utils/getValidationErrors';
+import { useAuth } from '../../hooks/auth';
 
 // Images
 import logoImg from '../../assets/logo.png';
@@ -30,13 +33,57 @@ import {
   CreateAccountButtonText,
 } from './styles';
 
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
   const passwordInputRef = useRef<TextInput>(null);
   const navigation = useNavigation();
 
-  const handleSignIn = useCallback((data: object) => {
-    console.log(data);
+  const { signIn, user } = useAuth();
+
+  console.log(user);
+
+  const handleSignIn = useCallback(async (data: SignInFormData) => {
+    try {
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().required('Senha obrigatória'),
+      });
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      await signIn({
+        email: data.email,
+        password: data.password,
+      });
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+
+        formRef.current?.setErrors(errors);
+
+        return;
+      }
+
+      Alert.alert(
+        'Erro na autenticacao',
+        'Verificar as credenciais de acesso.'
+      );
+      /*
+        addToast({
+          type: 'error',
+          title: 'Error ao tentar fazer o login',
+          description: 'Verificar as credenciais de acesso.',
+        }); */
+    }
   }, []);
 
   return (
@@ -62,10 +109,10 @@ const SignIn: React.FC = () => {
                 autoCorrect={false}
                 autoCapitalize="none"
                 keyboardType="email-address"
-                name="mail"
+                returnKeyType="next"
+                name="email"
                 icon="mail"
                 placeholder="Email"
-                returnKeyType="next"
                 onSubmitEditing={() => {
                   passwordInputRef.current?.focus();
                 }}
--- appbarber/src/routes/index.tsx
+++ appbarber/src/routes/index.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
+import AuthRoutes from './auth.routes';
+import AppRoutes from './app.routes';
 
-import { createStackNavigator } from '@react-navigation/stack';
+import { user } from '../hooks/auth';
 
-import SignIn from '../pages/SignIn';
-import SignUp from '../pages/SignUp';
+const Routes: React.FC = () => {
+  return <AuthRoutes />;
+};
 
-const Auth = createStackNavigator();
-
-const AuthRoutes: React.FC = () => (
-  <Auth.Navigator
-    screenOptions={{
-      headerShown: false,
-      cardStyle: { backgroundColor: '#312e38' },
-    }}
-  >
-    <Auth.Screen name="SignIn" component={SignIn} />
-    <Auth.Screen name="SignUp" component={SignUp} />
-  </Auth.Navigator>
-);
-
-export default AuthRoutes;
+export default Routes;
