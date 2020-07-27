8ceac92529945f1b40bf9db48ca3a801a6048677
Lucas96Sousa <lucas996oliveira@gmail.com>
2020-07-27 17:39:02 -0300

    ...

---
+++ server/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,33 @@
+import { injectable, inject } from 'tsyringe';
+
+// MODEL
+// import User from '@modules/users/infra/typeorm/entities/User';
+
+// ERROR
+// import AppError from '@shared/errors/AppError';
+
+// INTEFACE
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+export default class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+  ) {}
+
+  public async execute({ email }: IRequest): Promise<void> {
+    this.mailProvider.sendMail(
+      email,
+      'Pedido de recuperação de senha recebido',
+    );
+  }
+}
--- server/src/shared/container/providers/index.ts
+++ server/src/shared/container/providers/index.ts
@@ -1,5 +1,6 @@
 import { container } from 'tsyringe';
 
+// import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import IStorageProvider from './StoragedProviders/models/IstorageProvider';
 import DiskStorageProvider from './StoragedProviders/implementations/DiskStorageProvider';
 
